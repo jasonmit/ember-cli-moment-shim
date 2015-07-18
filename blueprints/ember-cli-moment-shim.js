bff6a0300e856e56ce02345fd97a3412b247fb13
Jason Mitchell <jason.mitchell.w@gmail.com>
2015-07-17 22:47:05 -0700

    adding support for timezone

---
+++ blueprints/ember-cli-moment-shim.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
+
+  afterInstall: function() {
+		// we need to install moment-timezone via bower since npmignore
+		// ignores `moment-timezone/builds/*`
+    return this.addBowerPackageToProject('moment-timezone');
+  }
+};
--- index.js
+++ index.js
@@ -3,30 +3,70 @@
 var path = require('path');
 var Funnel = require('broccoli-funnel');
 var mergeTrees = require('broccoli-merge-trees');
+var rename = require('broccoli-stew').rename;
 
 module.exports = {
   name: 'moment',
 
-  included: function(app) {
-    this._super.included(app);
-    app.import(path.join(this.treePaths.vendor, 'moment', 'min', 'moment.min.js'));
+  blueprintsPath: function blueprintsPath() {
+    return path.join(__dirname, 'blueprints');
   },
 
-  treeForVendor: function(vendorTree) {
+  included: function included(app) {
+    this._super.included(app);
+    var vendor = this.treePaths.vendor;
+    app.import(path.join(vendor, 'moment', 'min', 'moment.min.js'));
+    app.import(path.join(vendor, 'moment-timezone', 'tz.js'));
+  },
+
+  projectConfig: function projectConfig() {
+    return this.project.config(process.env.EMBER_ENV) || {};
+  },
+
+  treeForVendor: function treeForVendor(vendorTree) {
     var trees = [];
+    var app = this.app;
+    var options = this.projectConfig();
     var momentPath = path.dirname(require.resolve('moment'));
 
     if (vendorTree) {
       trees.push(vendorTree);
     }
 
-    trees.push(new Funnel(this.treeGenerator(momentPath), {
+    options = options.moment || {};
+
+    trees.push(new Funnel(momentPath, {
       destDir: 'moment',
       include: [new RegExp(/\.js$/)],
       exclude: ['tests', 'ender', 'package'].map(function(key) {
         return new RegExp(key + '\.js$');
       })
     }));
+
+    if (options.includeTimezone) {
+      var timezonePath = [this.project.bowerDirectory, 'moment-timezone', 'builds'];
+
+      switch(options.includeTimezone) {
+        case 'all':
+          timezonePath.push('moment-timezone-with-data.min.js');
+          break;
+        case '2010-2020':
+          timezonePath.push('moment-timezone-with-data-2010-2020.min.js');
+          break;
+        case 'none':
+          timezonePath.push('moment-timezone.min.js');
+          break;
+        default:
+          throw new Error("Ember Moment: Please specify the moment-timezone dataset to include as either 'all', '2010-2020', or 'none'.");
+          break;
+      }
+
+      trees.push(rename(new Funnel(path.join(this.project.bowerDirectory, 'moment-timezone', 'builds'), {
+        files: [timezonePath[timezonePath.length - 1]]
+      }), function(filepath) {
+        return path.join('moment-timezone', 'tz.js');
+      }));
+    }
 
     return mergeTrees(trees);
   }
