a872562b034bb78992753d5b465e264f7fe3e6ce
Jason Mitchell <jason.mitchell.w@gmail.com>
2015-07-30 20:35:58 -0700

    upgrading deps

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,7 +2,9 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {});
+  var app = new EmberApp(defaults, {
+    // Add options here
+  });
 
   /*
     This build file specifes the options for the dummy test app of this
