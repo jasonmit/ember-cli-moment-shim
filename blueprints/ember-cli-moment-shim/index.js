2668ede0cc7b80201ea627676e1e1d6ce9289f45
Richard Livsey <richard@livsey.org>
2016-03-02 17:12:41 +0000

    FastBoot Support
    Uses Node version of `moment-timezone` if running under FastBoot.
    
    The Node version of `moment-timezone` loads all locales/timezones 
    so there’s no need to honour any of the configuration.
    
    FastBoot requires its whitelisted dependencies to be in `dependencies` 
    vs `devDependencies` so moved them out of the blueprint.

--- blueprints/ember-cli-moment-shim/index.js
+++ blueprints/ember-cli-moment-shim/index.js
@@ -1,3 +1,4 @@
+/*jshint node:true*/
 'use strict';
 
 module.exports = {
@@ -5,12 +6,5 @@ module.exports = {
     // this prevents an error when the entityName is
     // not specified (since that doesn't actually matter
     // to us
-  },
-
-  afterInstall: function() {
-    return this.addPackagesToProject([
-      { name: 'moment', target: '^2.8.0' },
-      { name: 'moment-timezone', target: '^0.5.0' }
-    ]);
   }
 };
