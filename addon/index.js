91ba4f4a0e2b0904525faa495416030b34d24741
Jason Mitchell <jasonmit@nestlabs.com>
2016-12-14 13:04:16 -0800

    Compare as a constructor method

--- addon/index.js
+++ addon/index.js
@@ -4,7 +4,7 @@ import Ember from 'ember';
 
 const moment = self.moment;
 
-moment.prototype.compare = function compare(a, b) {
+function compare(a, b) {
   if (moment.isMoment(a) && moment.isMoment(b)) {
     if (a.isBefore(b)) {
       return -1;
@@ -18,10 +18,11 @@ moment.prototype.compare = function compare(a, b) {
   throw new Error('Arguments provided to `compare` are not moment objects');
 };
 
-if (!moment.prototype.hasOwnProperty('clone')) {
-  moment.prototype.clone = function clone() {
-    return self.moment(this);
-  }
+moment.prototype.compare = compare;
+moment.compare = compare;
+
+moment.prototype.clone = function clone() {
+  return self.moment(this);
 }
 
 export default moment;
--- tests/unit/utils/moment-export-test.js
+++ tests/unit/utils/moment-export-test.js
@@ -24,7 +24,6 @@ test('moment compare functions property', (assert) => {
   assert.equal(instanceB.compare(instanceB, instanceA), 1);
 });
 
-
 test('moment tz fn exists', (assert) => {
   assert.equal(typeof moment.tz, 'function');
 });
@@ -43,4 +42,23 @@ test('moment.utc is a class function', (assert) => {
 
 test('moment().clone is an instance function', (assert) => {
   assert.equal(typeof moment().clone, 'function');
+});
+
+test('ember computed sort can work with exported moment', (assert) => {
+  const ObjClass = Ember.Object.extend({
+    datesSortParam: ['date:asc'],
+    sortedDateContainers: Ember.computed.sort('dateContainers', function(a, b) {
+      return moment.compare(a.date, b.date);
+    })
+  });
+
+  const obj = ObjClass.create({dateContainers: Ember.A([
+    { date: moment(20000) },
+    { date: moment(0) },
+    { date: moment(10000) }
+  ])});
+
+  assert.equal(Number(obj.get('sortedDateContainers')[0].date), 0);
+  assert.equal(Number(obj.get('sortedDateContainers')[1].date), 10000);
+  assert.equal(Number(obj.get('sortedDateContainers')[2].date), 20000);
 });
