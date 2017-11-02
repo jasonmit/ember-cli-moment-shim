490ffbf80c0926b8b2c3a0724a56e9c62dda711d
Mikaël PIRIO <mikael@pirio.org>
2017-11-02 15:04:23 +0100

    Implemente `Ember.Copyable` in an 'augmentedMoment'

--- addon/index.js
+++ addon/index.js
@@ -1,4 +1,61 @@
 import moment from './lib';
+import Ember from 'ember';
+
+
+function hasOwnProp(a, b) {
+  return Object.prototype.hasOwnProperty.call(a, b);
+}
+
+function extend(a, b) {
+  for (let propName in b) {
+    if (hasOwnProp(b,propName)) {
+      Object.defineProperty(a,propName, {
+        enumerable: true,
+        configurable: true,
+        get() {
+          return b[propName];
+        },
+        set(newValue) {
+          b[propName] = newValue;
+        }
+      });
+    }
+  }
+
+  // non-enumerable property
+  ['toString', 'valueOf'].forEach(propName => {
+    if (hasOwnProp(b, propName)) {
+      Object.defineProperty(a, propName, {
+        enumerable: false,
+        configurable: true,
+        get() {
+          return b[propName];
+        },
+        set(newValue) {
+          b[propName] = newValue;
+        }
+      });
+    }
+  });
+
+  return a;
+}
+
+function createAugmentedMomentFrom(fn) {
+  const augmentedMoment = function() {
+    const m = fn(...arguments);
+    const meta = Ember.meta(m);
+    meta.writeMixins(Ember.guidFor(Ember.Copyable), Ember.Copyable);
+
+    return m;
+  }
+
+  augmentedMoment.prototype = moment.prototype;
+
+  extend(augmentedMoment, moment);
+
+  return augmentedMoment;
+}
 
 function compare(a, b) {
   if (moment.isMoment(a) && moment.isMoment(b)) {
@@ -18,7 +75,13 @@ moment.prototype.compare = compare;
 moment.compare = compare;
 
 moment.prototype.clone = function clone() {
-  return moment(this);
+  return augmentedMoment(this);
 }
 
-export default moment;
+moment.prototype.copy = function copy(deep) {
+  return this.clone();
+}
+
+const augmentedMoment = createAugmentedMomentFrom(moment);
+
+export default augmentedMoment;
