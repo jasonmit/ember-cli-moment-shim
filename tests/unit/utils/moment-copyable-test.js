df5f0ce3074c4e9ba66e81b6c3dc9944c07f811e
Mikaël PIRIO <mikael@pirio.org>
2017-11-02 15:02:17 +0100

    Add unit tests "implements Copyable mixin" on moment instance and its copy

--- tests/unit/utils/moment-copyable-test.js
+++ tests/unit/utils/moment-copyable-test.js
@@ -7,3 +7,27 @@ module('Unit | moment copyable');
 test('moment exports', (assert) => {
   assert.ok(moment, 'moment exports an object');
 });
+
+test('moment() implements Copyable mixin and "copy" fn exists', (assert) => {
+  const instance = moment();
+
+  assert.ok(Ember.Copyable.detect(instance));
+  assert.equal(typeof instance.copy, 'function');
+});
+
+test('moment()\'s copy implements Copyable mixin and "copy" fn exists', (assert) => {
+  const instance = moment();
+  const instanceCopy = Ember.copy(instance);
+
+  assert.ok(Ember.Copyable.detect(instanceCopy));
+  assert.equal(typeof instanceCopy.copy, 'function');
+})
+
+test('Copy a moment instance and modify it', (assert) => {
+  const instance = moment();
+  const instanceCopy = Ember.copy(instance);
+
+  instanceCopy.add(7, 'd');
+
+  assert.notOk(instanceCopy.isSame(instance), "Modified copy is not identical to the original.");
+})
